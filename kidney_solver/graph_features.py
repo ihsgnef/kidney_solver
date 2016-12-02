3fa22749f75d7d8bd8361745bc14bc7d233e2e2a
Denis Peskov <dap87@georgetown.edu>
2016-12-02 13:34:50 -0500

    Current version correctly returns outbound edges, num of vertices, and num of cycles.  Needs update on num of chains and inbound edges.  Future work needed on centrality

--- kidney_solver/graph_features.py
+++ kidney_solver/graph_features.py
@@ -15,9 +15,12 @@ class GraphFeatures:
         self.high_outdegree = 0
         self.high_indegree = 0
         self.centrality = 0 # ~~~How would one do this without networkx?~~~
-        self.num_verticies = graph.vertices
-        self.num_cycles = self.calculate_cycles()
-        self.num_chains = 0 # ~~~ need to update to calculate_chains when ndd issue resolved~~~
+        self.num_vertices = len(self.graph.get_digraph_vertices())
+        self.num_cycles = sum(self.calculate_cycles()) # can break this up by amount of each length of cycle as well
+        self.num_chains =  0#self.calculate_chains() # ~~~ need to update to calculate_chains when ndd issue resolved~~~
+
+        #populate the features
+        self.calculate_degrees()
 
         #kidney related features will be added later i.e
         # num of AB donors, location similarlity of donors+recipients, insurance similarity, etc.
@@ -25,13 +28,22 @@ class GraphFeatures:
     #finds avg and max edges of vertex in the graph
     def calculate_degrees(self):
 
-        avg_incoming, avg_outgoing = 0. #~~~float so decimal values aren't lost, or perhaps int would be better~~~
-        max_incoming, max_outgoing = 0
+        avg_incoming =  0. #~~~float so decimal values aren't lost, or perhaps int would be better~~~
+        avg_outgoing = 0.
+        max_incoming = 0
+        max_outgoing = 0
 
         #go through each vertex and calculate number of edges ~~~ how can I access edges from vertex? ~~~
-        for v in self.graph.vertices:
-            incoming = 0 #calculate in-bound vertices for each vertex
-            outgoing = 0 #calculate out-bound
+        for v in self.graph.get_digraph_vertices():
+            # calculate in-bound vertices for each vertex
+            incoming = 0
+            for other_v in self.graph.get_digraph_vertices():
+                try:
+                    if self.graph.digraph_edge_exists(other_v, v):
+                        incoming += 1
+                except:
+                    pass
+            outgoing = len(self.graph.get_digraph_edges(v)) #calculate out-bound
             avg_incoming += incoming
             avg_outgoing += outgoing
             if incoming > max_incoming:
@@ -40,17 +52,16 @@ class GraphFeatures:
                 max_outgoing = outgoing
 
         #set object variables to average and max
-        self.indegrees = avg_incoming/len(self.graph.vertices)
-        self.outdegrees = avg_outgoing / len(self.graph.vertices)
+        self.indegrees = avg_incoming/len(self.graph.get_digraph_vertices())
+        self.outdegrees = avg_outgoing / len(self.graph.get_digraph_vertices())
         self.high_indegree = max_incoming
         self.high_outdegree = max_outgoing
 
     def calculate_cycles(self):
         return count_cycles_and_chains.count_cycles(self.graph.digraph, MAX_CYCLE)
 
-    # ~~~currently doesn't work b/c line 67, in count_chains for e in ndd.edges: AttributeError: 'int' object has no attribute 'edges' ~~~
     def calculate_chains(self):
-        return count_cycles_and_chains.count_chains(self.graph.digraph, self.graph.ndds, MAX_CHAIN)
+        return count_cycles_and_chains.count_chains(self.graph.digraph, self.graph.get_ndds(), MAX_CHAIN)
 
     #~~~How to do this~~~
     def calculate_centrality(self):
@@ -67,7 +78,7 @@ class GraphFeatures:
             #feature_dict['centrality'] = self.centrality  ~~~not yet implemented
             feature_dict['max_in'] = self.high_indegree
             feature_dict['max_out'] = self.high_outdegree
-            feature_dict['num_vertices'] = self.num_verticies
+            feature_dict['num_vertices'] = self.num_vertices
             feature_dict['num_cycles'] = self.num_cycles
             feature_dict['num_chains'] = self.num_chains
 
